fae14b1acb1c6aff417112a5e4cab873c0c01ed6
vareyxu <vareyxu@tencent.com>
2022-05-04 20:04:19 +0800

    695. 岛屿的最大面积

---
+++ question/src/main/java/com/vareyxu/leetcode/editor/cn/MaxAreaOfIsland.java
@@ -0,0 +1,92 @@
+//给你一个大小为 m x n 的二进制矩阵 grid 。 
+//
+// 岛屿 是由一些相邻的 1 (代表土地) 构成的组合，这里的「相邻」要求两个 1 必须在 水平或者竖直的四个方向上 相邻。你可以假设 grid 的四个边缘都
+//被 0（代表水）包围着。 
+//
+// 岛屿的面积是岛上值为 1 的单元格的数目。 
+//
+// 计算并返回 grid 中最大的岛屿面积。如果没有岛屿，则返回面积为 0 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,
+//0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,
+//0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+//输出：6
+//解释：答案不应该是 11 ，因为岛屿只能包含水平或垂直这四个方向上的 1 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：grid = [[0,0,0,0,0,0,0,0]]
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == grid.length 
+// n == grid[i].length 
+// 1 <= m, n <= 50 
+// grid[i][j] 为 0 或 1 
+// 
+// Related Topics 深度优先搜索 广度优先搜索 并查集 数组 矩阵 👍 762 👎 0
+
+package com.vareyxu.leetcode.editor.cn;
+
+public class MaxAreaOfIsland{
+    public static void main(String[] args) {
+       Solution solution = new MaxAreaOfIsland().new Solution();
+    }
+
+class Solution {
+    private int result = 0;
+
+    public int maxAreaOfIsland(int[][] grid) {
+        // DFS深度优先搜索
+        if (grid.length == 0) {
+            return 0;
+        }
+
+        int max = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                result = 0;
+
+                if (grid[i][j] == 1) {
+                    dfs(grid, i, j);
+                }
+                max = Math.max(max, result);
+            }
+        }
+        return max;
+    }
+
+    public void dfs(int[][]grid, int r, int c) {
+        if (!isValid(grid, r, c)) {
+            return;
+        }
+        if (grid[r][c] != 1) {
+            return;
+        }
+        grid[r][c] = 2;
+        result++;
+
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+    private boolean isValid(int[][]grid, int r, int c) {
+        return 0 <= r && r < grid.length && 0 <= c && c < grid[0].length;
+    }
+}
+
+}
---
+++ question/src/test/java/com/vareyxu/leetcode/editor/cn/MaxAreaOfIslandTest.java
@@ -0,0 +1,23 @@
+package com.vareyxu.leetcode.editor.cn;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MaxAreaOfIslandTest {
+    @Test
+    public void testSolution() {
+        MaxAreaOfIsland.Solution solution = new MaxAreaOfIsland().new Solution();
+        int[][] input = new int[][]{
+                new int[]{0,0,1,0,0,0,0,1,0,0,0,0,0},
+                new int[]{0,0,0,0,0,0,0,1,1,1,0,0,0},
+                new int[]{0,1,1,0,1,0,0,0,0,0,0,0,0},
+                new int[]{0,1,0,0,1,1,0,0,1,0,1,0,0},
+                new int[]{0,1,0,0,1,1,0,0,1,1,1,0,0},
+                new int[]{0,0,0,0,0,0,0,0,0,0,1,0,0},
+                new int[]{0,0,0,0,0,0,0,1,1,1,0,0,0},
+                new int[]{0,0,0,0,0,0,0,1,1,0,0,0,0},
+        };
+        assertEquals(6, solution.maxAreaOfIsland(input));
+    }
+}
