f8d299fcb701e2f292527497ad9939e1eaaa5f47
vareyxu <vareyxu@tencent.com>
2022-05-05 15:19:11 +0800

    122. 买卖股票的最佳时机2

---
+++ question/src/test/java/com/vareyxu/leetcode/editor/cn/BestTimeToBuyAndSellStockIiTest.java
@@ -0,0 +1,18 @@
+package com.vareyxu.leetcode.editor.cn;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BestTimeToBuyAndSellStockIiTest {
+    @Test
+    public void testSolutionGreedy() {
+        BestTimeToBuyAndSellStockIi.Solution solution = new BestTimeToBuyAndSellStockIi().new Solution();
+        int[] input1 = new int[]{7,1,5,3,6,4};
+        int[] input2 = new int[]{1,2,3,4,5};
+        int[] input3 = new int[]{7,6,4,3,1};
+        assertEquals(7, solution.maxProfitGreedy(input1));
+        assertEquals(4, solution.maxProfitGreedy(input2));
+        assertEquals(0, solution.maxProfitGreedy(input3));
+    }
+}
