30a010ed6b5bb7e4b602b02a05cd4794beba131b
vareyxu <vareyxu@tencent.com>
2022-05-08 20:27:13 +0800

    36. 有效的数独

---
+++ question/src/test/java/com/vareyxu/leetcode/editor/cn/ValidSudokuTest.java
@@ -0,0 +1,36 @@
+package com.vareyxu.leetcode.editor.cn;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ValidSudokuTest {
+    @Test
+    public void testSolution() {
+        ValidSudoku.Solution solution = new ValidSudoku().new Solution();
+        char[][] input1 = new char[][]{
+                new char[]{'5','3','.','.','7','.','.','.','.'},
+                new char[]{'6','.','.','1','9','5','.','.','.'},
+                new char[]{'.','9','8','.','.','.','.','6','.'},
+                new char[]{'8','.','.','.','6','.','.','.','3'},
+                new char[]{'4','.','.','8','.','3','.','.','1'},
+                new char[]{'7','.','.','.','2','.','.','.','6'},
+                new char[]{'.','6','.','.','.','.','2','8','.'},
+                new char[]{'.','.','.','4','1','9','.','.','5'},
+                new char[]{'.','.','.','.','8','.','.','7','9'}
+        };
+        char[][] input2 = new char[][]{
+                new char[]{'8','3','.','.','7','.','.','.','.'},
+                new char[]{'6','.','.','1','9','5','.','.','.'},
+                new char[]{'.','9','8','.','.','.','.','6','.'},
+                new char[]{'8','.','.','.','6','.','.','.','3'},
+                new char[]{'4','.','.','8','.','3','.','.','1'},
+                new char[]{'7','.','.','.','2','.','.','.','6'},
+                new char[]{'.','6','.','.','.','.','2','8','.'},
+                new char[]{'.','.','.','4','1','9','.','.','5'},
+                new char[]{'.','.','.','.','8','.','.','7','9'}
+        };
+        assertTrue(solution.isValidSudoku(input1));
+        assertFalse(solution.isValidSudoku(input2));
+    }
+}
